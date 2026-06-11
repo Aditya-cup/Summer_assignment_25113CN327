22c675fee60baa3840869319094416fd293030d6
Aditya-cup <25113cn327@glbitm.ac.in>
2026-06-11 20:08:43 +0530

    Create binary_to_decimal.c

---
+++ day6/binary_to_decimal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    int n, temp, decimal = 0, base = 1, r;
+    printf("Enter any binary number: ");
+    scanf("%d", &n);
+    temp = n; 
+
+    while (n > 0) 
+    {
+        r = n % 10;          
+        decimal = decimal + r * base;
+        n = n / 10;           
+        base = base * 2;       
+    }
+    printf("Decimal number = %d\n", decimal);
+    return 0;
+}
